334c13e1715a711d5302a0622d33718fa84c64e0
Kyosohee18 <hanashahky@gmail.com>
2025-06-26 11:06:37 +0700

    constructor

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 using namespace std;
 
-BinarySearchTree_0046
+//BinarySearchTree_0046
+
+class Node 
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    //contructor for the node class
+
+}
